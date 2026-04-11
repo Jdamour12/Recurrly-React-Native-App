8f6a31f048f865c325237071982e027d14a17b68
Jdamour12 <jeandamourkubwimana0@gmail.com>
2026-04-11 12:02:54 +0200

    Solved className attributes issues

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -7,7 +7,7 @@ const SafeAreaView = styled(RNSafeAreaView);
 
 const Settings = () => {
     return (
-        <SafeAreaView className="flex-1 bg-background, p-5">
+        <SafeAreaView className="flex-1 bg-background p-5">
             <Text>Settings</Text>
         </SafeAreaView>
     );
